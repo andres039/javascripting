cce841b8b7ffa3d64257e0ba891f62874884b5d7
Andres <andres039039@Hotmail.com>
2021-10-23 14:59:06 +0000

    modified

--- functions.js
+++ functions.js
@@ -1,4 +1,33 @@
-function eat (food) {
-  return food + ' tasted really good.' 
+// function eat (food) {
+//   return food + ' tasted really good.'
+// }
+// console.log(eat('bananas'))
+
+function judgeVegetable(vegetables, metric) {
+  let metrics = vegetables.map((vegetable) => vegetable[metric]);
+  let highestMark = Math.max(...metrics);
+  return vegetables[metrics.indexOf(highestMark)].submitter;
+  // return vegetables.submitter
 }
-console.log(eat('bananas'))
+console.log(
+  judgeVegetable(
+    [
+      {
+        submitter: "Old Man Franklin",
+        redness: 10,
+        plumpness: 5,
+      },
+      {
+        submitter: "Sally Tomato-Grower",
+        redness: 2,
+        plumpness: 8,
+      },
+      {
+        submitter: "Hamid Hamidson",
+        redness: 4,
+        plumpness: 3,
+      },
+    ],
+    "redness"
+  )
+);
--- string-length.js
+++ string-length.js
@@ -1,2 +1,3 @@
+'use strict';
 const example = 'example string'
 console.log(example.length)
